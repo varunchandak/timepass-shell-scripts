d04cd1139d805897bda6e9bd6cee9dc5d2eb152f
Varun Chandak <varunchandak@Varuns-MacBook-Air.local>
2022-11-27 18:48:23 +0530

    attributes

--- genPass.sh
+++ genPass.sh
@@ -6,8 +6,11 @@
 # To change the character limit, change the CHAR_LIMIT variable
 # To change the number of passwords generated, change the PASS_COUNT variable
 
-CHAR_LIMIT=20
-PASS_COUNT=20
+
+export CUSTOM_CHAR_LIMIT="$1"
+export CUSTOM_PASS_COUNT="$2"
+CHAR_LIMIT=${CUSTOM_CHAR_LIMIT:-20}
+PASS_COUNT=${CUSTOM_PASS_COUNT:-20}
 
 for i in `seq $PASS_COUNT`
 	do cat /dev/urandom| tr -dc 'a-zA-Z0-9-_@#^'|fold -w "$CHAR_LIMIT" | head -n 1
